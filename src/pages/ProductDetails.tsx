0c6cc6f0e7bc5756f12b4a403c7d74e35b7f0894
Igli-Boci <igliboci10@gmai.com>
2022-08-14 14:44:51 +0200

    Adding products and products details

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../types";
+
 export function ProductDetails() {
+  const [product, setProduct] = useState<Product | null>(null);
+  const params = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:3010/products/${params.id}`)
+      .then((resp) => resp.json())
+      .then((productFromServer) => setProduct(productFromServer));
+  }, []);
+
+  if (product === null) return <h2>Loading...</h2>;
+
   return (
     <div>
-      <h1>Product id</h1>
+      <section className="product-detail main-wrapper">
+        <img src={product.image} alt={product.title} />
+        <div
+          className="product-detail__side"
+          style={{ borderColor: "var(--yellow)" }}
+        >
+          <h3></h3>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>£{product.price}</p>
+          <button>Add to basket</button>
+        </div>
+      </section>
     </div>
   );
 }
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Product } from "../types";
 
 export function Products() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect (() => {
-    fetch('http://localhost:3010/products')
-    .then(resp => resp.json())
-    .then(productsFromServer => setProducts(productsFromServer))
-  }, [])
+  useEffect(() => {
+    fetch("http://localhost:3010/products")
+      .then((resp) => resp.json())
+      .then((productsFromServer) => setProducts(productsFromServer));
+  }, []);
   return (
     <div>
-      <h1>Products</h1>
+      <section className="products-container main-wrapper">
+        <ul className="products-container__list">
+          {products.map((product) => (
+            <li>
+              <Link to={`/products/${product.id}`}>
+                <article className="product-item">
+                  <img src={product.image} alt={product.title} />
+                  <h3>{product.title}</h3>
+                </article>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
